81d2b9e56300a6aa5b85b426a4d03251fa988a9f
brad <brad@brad.tw>
2019-11-08 13:34:28 +0800

    1

--- app/src/main/java/tw/org/iii/HelloWorld/MainActivity.java
+++ app/src/main/java/tw/org/iii/HelloWorld/MainActivity.java
@@ -3,12 +3,28 @@ package tw.org.iii.HelloWorld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button buttonClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.lottery);
+        buttonClick = findViewById(R.id.click);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("brad", "OK");
+            }
+        });
+
     }
 }
